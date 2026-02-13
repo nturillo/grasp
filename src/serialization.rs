a94d40a1ccfd1957ac8f45a11bc18e95cf7b397b
Justin Berke <justinberke0406@gmail.com>
2026-02-13 16:07:41 -0500

    tgf and gml

--- src/serialization.rs
+++ src/serialization.rs
@@ -1,10 +1,10 @@
-use crate::graph::graph_traits::{GraphTrait, VertexType};
+use std::ops::Add;
+
 use crate::graph::adjacency_list::SparseGraph;
-
-
+use crate::graph::graph_traits::{GraphTrait, VertexType};
 
 pub fn to_dot<G: GraphTrait>(g: G) -> String {
-    let mut s= "graph {\n".to_string();
+    let mut s = "graph {\n".to_string();
     let mut verts: Vec<VertexType> = g.vertices().collect();
     verts.sort();
     for v in verts {
@@ -13,10 +13,65 @@ pub fn to_dot<G: GraphTrait>(g: G) -> String {
     s.push_str("\n");
     let mut edges: Vec<(VertexType, VertexType)> = g.edges().collect();
     edges.sort();
-    for (u,v) in edges {
+    for (u, v) in edges {
         s.push_str(&format!("    {} -- {};\n", u, v))
     }
     s.push_str("}");
+    s
+}
+
+pub fn to_tgf<G: GraphTrait>(g: G) -> String {
+    let mut s = String::new();
+    let mut verts: Vec<VertexType> = g.vertices().collect();
+    verts.sort();
+    for v in verts {
+        s.push_str(&format!("{}\n", v));
+    }
+    s.push_str("#\n");
+    let mut edges: Vec<(VertexType, VertexType)> = g.edges().collect();
+    edges.sort();
+    for (u, v) in edges {
+        s.push_str(&format!("{} {}\n", u, v))
+    }
+    s.pop();
+    s
+}
+
+pub fn to_gml<G: GraphTrait>(g: G) -> String {
+    let mut s = "graph [\n".to_string();
+    let mut index = 0;
+
+    let mut push =
+        |string: String, index: &usize| s.push_str(&"\t".repeat(*index).add(&string).add("\n"));
+
+    index += 1;
+
+    let mut verts: Vec<VertexType> = g.vertices().collect();
+    verts.sort();
+
+    for v in verts {
+        push("node [".to_string(), &index);
+        index += 1;
+
+        push(format!("id {}", v), &index);
+        index -= 1;
+        push("]".to_string(), &index);
+    }
+
+    let mut edges: Vec<(VertexType, VertexType)> = g.edges().collect();
+    edges.sort();
+
+    for e in edges {
+        push("edge [".to_string(), &index);
+        index += 1;
+
+        push(format!("source {}", &e.0), &index);
+        push(format!("target {}", &e.1), &index);
+        index -= 1;
+        push("]".to_string(), &index);
+    }
+    index -= 1;
+    s.push_str(&"]".to_string());
     s
 }
 
@@ -27,13 +82,13 @@ mod tests {
     #[test]
     fn butterfly_dot() {
         let mut butterfly = SparseGraph::new();
-        butterfly.add_edge((1,2));
-        butterfly.add_edge((2,3));
-        butterfly.add_edge((1,3));
-        butterfly.add_edge((1,4));
-        butterfly.add_edge((1,5));
-        butterfly.add_edge((4,5));
-        
+        butterfly.add_edge((1, 2));
+        butterfly.add_edge((2, 3));
+        butterfly.add_edge((1, 3));
+        butterfly.add_edge((1, 4));
+        butterfly.add_edge((1, 5));
+        butterfly.add_edge((4, 5));
+
         let butterfly_dot = "graph {\
         \n    1;\
         \n    2;\
@@ -48,7 +103,89 @@ mod tests {
         \n    2 -- 3;\
         \n    4 -- 5;\
         \n}";
-       
+
         pretty_assertions::assert_eq!(butterfly_dot, to_dot(butterfly));
+    }
+
+    #[test]
+    fn butterfly_tgf() {
+        let mut butterfly = SparseGraph::new();
+        butterfly.add_edge((1, 2));
+        butterfly.add_edge((2, 3));
+        butterfly.add_edge((1, 3));
+        butterfly.add_edge((1, 4));
+        butterfly.add_edge((1, 5));
+        butterfly.add_edge((4, 5));
+
+        let butterfly_tgf = "\
+        1\n\
+        2\n\
+        3\n\
+        4\n\
+        5\n\
+        #\n\
+        1 2\n\
+        1 3\n\
+        1 4\n\
+        1 5\n\
+        2 3\n\
+        4 5";
+
+        pretty_assertions::assert_eq!(butterfly_tgf, to_tgf(butterfly));
+    }
+
+    #[test]
+    fn butterfly_gml() {
+        let mut butterfly = SparseGraph::new();
+        butterfly.add_edge((1, 2));
+        butterfly.add_edge((2, 3));
+        butterfly.add_edge((1, 3));
+        butterfly.add_edge((1, 4));
+        butterfly.add_edge((1, 5));
+        butterfly.add_edge((4, 5));
+
+        let butterfly_gml = "graph [\
+            \n\tnode [\
+            \n\t\tid 1\
+            \n\t]\
+            \n\tnode [\
+            \n\t\tid 2\
+            \n\t]\
+            \n\tnode [\
+            \n\t\tid 3\
+            \n\t]\
+            \n\tnode [\
+            \n\t\tid 4\
+            \n\t]\
+            \n\tnode [\
+            \n\t\tid 5\
+            \n\t]\
+            \n\tedge [\
+            \n\t\tsource 1\
+            \n\t\ttarget 2\
+            \n\t]\
+            \n\tedge [\
+            \n\t\tsource 1\
+            \n\t\ttarget 3\
+            \n\t]\
+            \n\tedge [\
+            \n\t\tsource 1\
+            \n\t\ttarget 4\
+            \n\t]\
+            \n\tedge [\
+            \n\t\tsource 1\
+            \n\t\ttarget 5\
+            \n\t]\
+            \n\tedge [\
+            \n\t\tsource 2\
+            \n\t\ttarget 3\
+            \n\t]\
+            \n\tedge [\
+            \n\t\tsource 4\
+            \n\t\ttarget 5\
+            \n\t]\
+            \n]";
+
+        pretty_assertions::assert_eq!(butterfly_gml, to_gml(butterfly));
     }
 }
