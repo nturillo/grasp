0294be4e42ba482fcbcfa6f0cb535606ba3b2bb1
Justin Berke <justinberke0406@gmail.com>
2026-03-08 03:27:36 -0400

    strong connected components and tests

--- grasp/src/algorithms/connectivity.rs
+++ grasp/src/algorithms/connectivity.rs
@@ -1,6 +1,6 @@
-use std::collections::{BTreeMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 
-use crate::{algorithms::algo_traits::AlgoTrait, graph::{DiGraph, SimpleGraph, UnderlyingGraph, VertexID}};
+use crate::{algorithms::algo_traits::AlgoTrait, graph::{DiGraph, Set, SimpleGraph, UnderlyingGraph, VertexID}};
 
 /// Determine if a simple graph is connected.
 pub fn is_connected<G: SimpleGraph>(g: &G) -> bool {
@@ -17,48 +17,40 @@ pub fn is_strongly_connected<G: DiGraph>(g: &G) -> bool {
     strongly_connected_components(g).len() == 1
 }
 
-pub fn strongly_connected_components<G: DiGraph>(g: &G) -> Vec<HashSet<VertexID>> {
+/// Return the strongly connected components of a digraph.
+pub fn strongly_connected_components<G: DiGraph>(g: &G) -> Vec<G::VertexSet> {
+    #[derive(Clone, Copy)]
     struct VertexWrapper {
-        pub disc: Option<u32>,
-        pub low: Option<u32>,
+        pub disc: u32,
+        pub low: u32,
         pub on_stack: bool,
     }
 
     let mut stack = vec![];
     let mut index = 0;
-    let mut vertex_map = BTreeMap::new();
-    let mut comps: Vec<HashSet<VertexID>> = Vec::new();
+    let mut vertex_map: HashMap<VertexID, VertexWrapper> = HashMap::new();
+    let mut comps: Vec<G::VertexSet> = Vec::new();
 
-    for vertex in g.vertices() {
-        vertex_map.insert(vertex, VertexWrapper {
-            disc: None,
-            low: None,
-            on_stack: false,
-        });
-    }
-
-    fn visit<G: DiGraph>(index: &mut u32, vertex_id: VertexID, g: &G, vertex_map: &mut BTreeMap<usize, VertexWrapper>, stack: &mut Vec<VertexID>, comps: &mut Vec<HashSet<VertexID>>) {
-        let vertex = vertex_map.get_mut(&vertex_id).unwrap();
-
-        vertex.disc = Some(*index);
-        vertex.low = Some(*index);
-
+    fn visit<G: DiGraph>(index: &mut u32, vertex_id: VertexID, g: &G, vertex_map: &mut HashMap<VertexID, VertexWrapper>, stack: &mut Vec<VertexID>, comps: &mut Vec<G::VertexSet>) {
+        vertex_map.insert(vertex_id, VertexWrapper { disc: *index, low: *index, on_stack: true });
         *index += 1;
-
         stack.push(vertex_id);
-        vertex.on_stack = true;
 
-        for target_id in g.neighbors(vertex_id).unwrap().into_owned().into_iter() {
-            if vertex_map.get(&target_id).unwrap().disc.is_none() {
+        for target_id in g.out_neighbors(vertex_id).unwrap().into_owned().into_iter() {
+            if let Some(target) = vertex_map.get(&target_id).copied() {
+                if target.on_stack {
+                    let vertex = vertex_map.get_mut(&vertex_id).unwrap();
+                    vertex.low = vertex.low.min(target.disc);
+                }
+            } else {
                 visit(index, target_id, g, vertex_map, stack, comps);
-                vertex_map.get_mut(&vertex_id).unwrap().low = vertex_map.get_mut(&vertex_id).unwrap().low.min(vertex_map.get(&target_id).unwrap().low);
-            } else if  vertex_map.get(&target_id).unwrap().on_stack {
-                vertex_map.get_mut(&vertex_id).unwrap().low = vertex_map.get_mut(&vertex_id).unwrap().low.min(vertex_map.get(&target_id).unwrap().disc);
+                let target = vertex_map.get(&target_id).copied().unwrap();
+                let vertex = vertex_map.get_mut(&vertex_id).unwrap();
+                vertex.low = vertex.low.min(target.low);
             }
         }
 
         let vertex = vertex_map.get_mut(&vertex_id).unwrap();
-
         if vertex.low == vertex.disc {
             let mut scc = HashSet::new();
 
@@ -72,15 +64,91 @@ pub fn strongly_connected_components<G: DiGraph>(g: &G) -> Vec<HashSet<VertexID>
                 }
             };
 
-            comps.push(scc);
+            comps.push(scc.iter().copied().collect());
         }
     }
 
     for vertex in g.vertices() {
-        if vertex_map.get(&vertex).unwrap().disc.is_none() {
+        if !vertex_map.contains_key(&vertex) {
             visit(&mut index, vertex, g, &mut vertex_map, &mut stack, &mut comps);
         }
     }
 
     comps
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{algorithms::connectivity::*, graph::{GraphTrait, prelude::{SparseDiGraph, SparseSimpleGraph}}};
+
+    #[test]
+    pub fn empty_simple_connected() {
+        let graph = SparseSimpleGraph::default();
+        pretty_assertions::assert_eq!(false, is_connected(&graph));
+    }
+
+    #[test]
+    pub fn simple_connected() {
+        let mut graph = SparseSimpleGraph::default();
+        graph.add_edge((1, 2));
+        graph.add_edge((2, 3));
+        graph.add_edge((2, 4));
+        pretty_assertions::assert_eq!(true, is_connected(&graph));
+    }
+
+    #[test]
+    pub fn simple_not_connected() {
+        let mut graph = SparseSimpleGraph::default();
+        graph.add_edge((1, 2));
+        graph.add_edge((2, 3));
+        graph.add_vertex(4);
+        pretty_assertions::assert_eq!(false, is_connected(&graph));
+    }
+
+    #[test]
+    pub fn digraph_only_weak() {
+        let mut graph = SparseDiGraph::default();
+        graph.add_edge((1, 2));
+        graph.add_edge((2, 3));
+        graph.add_edge((2, 4));
+        pretty_assertions::assert_eq!(true, is_weakly_connected(&graph));
+        pretty_assertions::assert_eq!(false, is_strongly_connected(&graph));
+    }
+
+    #[test]
+    pub fn digraph_strongly_connected() {
+        let mut graph = SparseDiGraph::default();
+        graph.add_edge((1, 2));
+        graph.add_edge((2, 3));
+        graph.add_edge((3, 1));
+        pretty_assertions::assert_eq!(true, is_strongly_connected(&graph));
+    }
+
+    #[test]
+    pub fn digraph_not_connected() {
+        let mut graph = SparseDiGraph::default();
+        graph.add_edge((1, 2));
+        graph.add_edge((2, 3));
+        graph.add_vertex(4);
+        pretty_assertions::assert_eq!(false, is_weakly_connected(&graph));
+    }
+
+    #[test]
+    pub fn digraph_ssc() {
+        let mut graph = SparseDiGraph::default();
+        graph.add_edge((1, 2));
+        graph.add_edge((2, 3));
+        graph.add_edge((2, 4));
+        graph.add_edge((3, 1));
+        graph.add_edge((4, 5));
+        graph.add_edge((5, 4));
+        graph.add_edge((6, 1));
+        let sscs = strongly_connected_components(&graph);
+        let v1: HashSet<VertexID> = HashSet::from([6]);
+        let v2: HashSet<VertexID> = HashSet::from([1, 2, 3]);
+        let v3: HashSet<VertexID> = HashSet::from([4, 5]);
+        pretty_assertions::assert_eq!(true, sscs.contains(&v1));
+        pretty_assertions::assert_eq!(true, sscs.contains(&v2));
+        pretty_assertions::assert_eq!(true, sscs.contains(&v3));
+    }
 }
